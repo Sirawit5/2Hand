103224fa077478b4f54ea9227eed6221583cebe6
supai_n <supai.n@ku.th>
2025-10-04 22:27:48 +0700

    daas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'sign-up', component: SignupPageComponent },  
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'sign-up', component: SignupPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // ให้หน้าแรกไป home
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
